4d691c43b77c13ca7caba4b1d4f9579d1f538298
RyanL0213 <tui02780@temple.edu>
2020-09-16 19:37:12 -0400

    added a button to the project // set onclicklistener and print message

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.temple.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.mybutton);
+
+        myButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+            Toast.makeText(MainActivity.this,"You have clicked the button",Toast.LENGTH_SHORT).show();
+        }
+
+
+        });
+    }
+}
